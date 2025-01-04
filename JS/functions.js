877dd8c45b4328b227cf2f3a88450df69f66af41
Draguisada <draguisada@gmail.com>
2025-01-04 18:42:18 -0300

    Feito!

--- JS/functions.js
+++ JS/functions.js
@@ -7,9 +7,100 @@ function validarValores () {
 }
 
 function rolarDados() {
-    let valores = validarValores();
 
-    console.log(valores);
+    if (!manterDados.checked) {
+        limparMesa();
+    }
+
+    let rolarDados = validarValores();
+    let resultados = [];
+    // Rolar todos os dados e identificando qual é X6, X10 ou X12.
+    for(i=0;i<rolarDados.x6;i++) {
+        resultados.push(['x6', randIntInc(1, 6)]);
+    }
+    for(i=0;i<rolarDados.x10;i++) {
+        resultados.push(['x10', randIntInc(1, 10)]);
+    }
+    for(i=0;i<rolarDados.x12;i++) {
+        resultados.push(['x12', randIntInc(1, 12)]);
+    }
+
+    //Transformar os valores em "simbolos"
+    for(i=0;i<resultados.length;i++) {
+        resultados[i][1] = tabelaDados[resultados[i][1]]
+    }
+
+    historico.push(resultados);
+
+    mostrarMesa(resultados);
+
+}
+
+function simbolToEmoji(string) {
+    let newString = '';
+    for(i=0;i<string.length;i++) {
+        switch (string[i]){
+            case'!': newString += '🐞'; break;
+            case '?': newString += '🦌'; break;
+            case '#': newString += '🐺'; break;
+            default: break;
+        }
+    };
+    return newString;
+}
+
+function descreverSimbolos(string) {
+    let sucessos = 0;
+    let adaptacao = 0;
+    let pressao = 0;
+    for(i=0;i<string.length;i++) {
+        switch (string[i]){
+            case'!': sucessos++; break;
+            case '?': adaptacao++; break;
+            case '#': pressao++; break;
+            default: break;
+        }
+    };
+    let newString = `${sucessos} Sucessos <br> ${adaptacao} Adaptação <br> ${pressao} Pressões <br> ${string} `;
+    return newString;
+}
+
+function limparMesa() {
+    let crionca = mesa.children;
+    let lencrionca = crionca.length;
+    for(i=0;i<lencrionca;i++){
+        mesa.removeChild(crionca[0]);
+    };
+}
+
+function mostrarMesa(dados) {
+    /* 
+    Criar objeto
+    const bloco = document.createElement('div');
+    Modificar objeto
+    bloco.classList.add('free');
+    Adicionar o Objeto ao HTML
+    mesa.appendChild(bloco);
+    */
+   dados.forEach(element => {
+        const dado = document.createElement('div');
+        const desc = document.createElement('div');
+        desc.classList.add('desc');
+
+        // O simbolo do dado (Se vai aparecer q é um D6, D10 ou D12)
+        switch (element[0]){
+            case 'x6': dado.classList.add('x6'); break;
+            case 'x10': dado.classList.add('x10'); break;
+            default: dado.classList.add('x12'); break;
+        }
+
+        dado.textContent = simbolToEmoji(element[1]);
+        desc.innerHTML = descreverSimbolos(element[1]);
 
 
+        dado.appendChild(desc);
+        mesa.appendChild(dado);
+
+   });
+    
 }
--- JS/main.js
+++ JS/main.js
@@ -3,17 +3,43 @@ const mesa = document.getElementById('mesa');
 const quantX6 = document.getElementById('x6');
 const quantX10 = document.getElementById('x10');
 const quantX12 = document.getElementById('x12');
+const limparResults = document.getElementById('limparResult');
+const manterDados = document.getElementById('manterDado');
 
-const roll = document.getElementById('roll')
+const roll = document.getElementById('roll');
 
-roll.addEventListener('click', rolarDados)
+const tabelaDados = {
+    1: '',
+    2: '',
+    3: '#',
+    4: "#?",
+    5: "#?",
+    6: '!',
+    7: "!!",
+    8: "!?",
+    9: "!?#",
+    10: "!!#",
+    11: "!??#",
+    12: "##"
+}
 
-/* 
-Criar objeto
-const bloco = document.createElement('div');
-Modificar objeto
-bloco.classList.add('free');
-Adicionar o Objeto ao HTML
-mesa.appendChild(bloco);
+let historico = [];
 
-*/
+roll.addEventListener('click', rolarDados);
+
+// Quando clica em um dado ele remove o dado.
+mesa.addEventListener('click', event => {
+    mesa.removeChild(event.target);
+});
+
+limparResults.addEventListener('click', limparMesa);
+
+// Funções globais
+// Random intiger (Que é incluso) que pode ser um intervalo mas pode ser 0 até max
+function randIntInc(min, max=null) {
+    if (max === null) {
+        max = min+1;
+        min = 0;
+    } else {max++};
+    return Math.floor(Math.random() * (max - min) + min);
+}
